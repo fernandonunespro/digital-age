5c449e0c92bf5f89b05e78da6f0be1bcfc9bfa1a
Fernando Nunes de Almeida <nunesfernandopro@gmail.com>
2023-05-21 18:18:53 +0100

    commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function verificar(){
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  if (fano.value.length == 0 || fano.value > ano) {
+    window.alert('[ERRO] Verifique os dados e tente novamente!');
+  }
+  else{
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    res.innerHTML = `Idade Calculada: ${idade}`
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked){
+      genero = 'Homem'
+      if (idade >=0 && idade < 10){
+            //crianca
+            img.setAttribute('src', './images/babyboy.jpg')
+      } else if (idade < 21){
+            //Jovem
+            img.setAttribute('src', './images/maleteenager.jpg')
+      } else if (idade < 50) {
+            //Adulto
+            img.setAttribute('src', './images/man.jpg')
+        } else {
+            //idoso
+            img.setAttribute('src', './images/oldestman.jpg')
+        }
+      
+      } else if (fsex[1].checked){
+      genero = 'Mulher'
+      if (idade >=0 && idade < 10) {
+         //crianca
+         img.setAttribute('src', './images/babygirl.jpg')
+      } else if (idade < 21) {
+         //Jovem
+         img.setAttribute('src', './images/femaleteenager.jpg')
+      } else if (idade < 50) {
+        //Adulto
+        img.setAttribute('src', './images/woman.jpg')
+      } else {
+        //idoso
+        img.setAttribute('src', './images/oldestwoman.jpg')
+      }
+    }
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
+  }
+
+}
